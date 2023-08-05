9e6a4e2df3e5ec34ea3ef4ac88bf05cd27252948
Therobo77 <anshusinha8873@gmail.com>
2023-08-05 15:15:26 +0530

    ok

--- episode4/first-food-app/src/Pages/Body.jsx
+++ episode4/first-food-app/src/Pages/Body.jsx
@@ -6,9 +6,9 @@ import Shinner from "./Shinner";
 
 const Body = () => {
   const [restaurantdata, setRestaurantdata] = useState([]);
-  //   const filterFun= () =>{
-  // console.log(allrestaurants.length) //
-  //   }
+  const [filterdata, setfilterdata] = useState([]);
+
+  const [searchdata, setsearchdata] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -17,34 +17,62 @@ const Body = () => {
     let data = await fetch("https://dummyjson.com/products");
     let jsondata = await data.json();
     setRestaurantdata(jsondata.products);
+    setfilterdata(jsondata.products);
     // console.log(jsondata);
   };
   return (
     <div>
       <div className="search-and-filter">
-        <button
-          onClick={() => {
-            //  filterFun
-            // console.log(allrestaurants.length)
-            let filterlist = restaurantdata.filter((res) => res.rating >= 4.5);
-            setRestaurantdata(filterlist);
-          }}
-        >
-          Filter by 3-4 Rating
-        </button>
-        <button
-          onClick={() => {
-            setRestaurantdata(restaurantdata);
-          }}
-        >
-          Reset
-        </button>
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search Here"
+            value={searchdata}
+            onChange={(e) => setsearchdata(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              console.log(restaurantdata[0].title);
+              let filterlist = restaurantdata.filter((res) => {
+                return res.title
+                  .toLowerCase()
+                  .includes(searchdata.toLowerCase());
+              });
+              setfilterdata(filterlist);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <br />
+        <div className="filter">
+          <button
+            onClick={() => {
+              //  filterFun
+              // console.log(allrestaurants.length)
+              let filterlist = restaurantdata.filter(
+                (res) => res.rating >= 4.5
+              );
+              setRestaurantdata(filterlist);
+            }}
+          >
+            Top Rated
+          </button>
+          <button
+            onClick={() => {
+              setRestaurantdata(restaurantdata);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+        <br />
       </div>
       <div className="restaurant-card">
         {restaurantdata.length === 0 ? (
           <Shinner />
         ) : (
-          restaurantdata.map((el, i) => {
+          filterdata.map((el, i) => {
             return <RestaurantCard key={i} resdata={el} />;
           })
         )}
